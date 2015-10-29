f306a85b3652a71e1d423ded198646a07d21e313
UnawareAlex <agarnold@NN123-HA-10.HS.sd203.org>
2015-10-29 14:14:38 -0500

    10/29
    Substring and Diamond programs

---
+++ Practice C6/Diamond.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Diamond
+{
+    public static void Diamond()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int side = scan.nextInt();
+        int num = (side * 2) - 1; //Number of rows, max number of stars and spaces
+        String space = " ";
+        String maxSpaces = "";
+        for (int i = 1; i <= num; i++)
+        {
+            maxSpaces += space;
+
+        
+        for (int n = 0; n < num; n++)
+        {
+           
+        }
+    }
+    System.out.println(maxSpaces);
+}
+}
+    
--- Practice C6/Substrings.java
+++ Practice C6/Substrings.java
@@ -4,7 +4,24 @@ public class Substrings
 {
     public static void substrings()
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a word: ");
+        String input = scan.next();
         
+        for (int i = 1; i <= input.length(); i++)
+        {
+            int start = 0;
+            int end = i;
+            
+            while (end <= input.length())
+            {
+                String sub = input.substring(start,end);
+                System.out.println(sub);
+                start++;
+                end++;
+           
+            }
+        }
     }
 }
     
