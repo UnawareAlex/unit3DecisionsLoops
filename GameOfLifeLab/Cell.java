9c8f0dc375cdc2c8f90b7737abeef95df6f875af
UnawareAlex <agarnold@NN123-HA-10.HS.sd203.org>
2015-11-17 14:13:12 -0600

    11/17 (In class edits)
    Changed my image of actor and started making my coordinates into an
    array

---
+++ GameOfLifeLab/Cell.java
@@ -0,0 +1,18 @@
+import info.gridworld.actor.Actor;
+
+/**
+ * Write a description of class Cell here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cell extends Actor
+{
+   public void act()
+   {
+   }
+   public Cell()
+   {
+       setColor(null);
+   }
+}
--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -1,6 +1,5 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.actor.Rock;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.BoundedGrid;
 import info.gridworld.grid.Location;
@@ -81,115 +80,117 @@ public class GameOfLife
         final int X25 = 6, Y25 = 14;
         final int X26 = 13, Y26 = 14;
 
-
+        //Int[] xArray;
+        //xArray = new Int[26];
+        //xArray[0] = 
         // the grid of Actors that maintains the state of the game
         //  (alive cells contains actors; dead cells do not)
         Grid<Actor> grid = world.getGrid();
         
-        // create and add rocks (a type of Actor) to the three intial locations
-        Rock rock1 = new Rock();
+        // create and add cells (a type of Actor) to the three intial locations
+        Cell cell1 = new Cell();
         Location loc1 = new Location(Y1, X1);
-        grid.put(loc1, rock1);
+        grid.put(loc1, cell1);
         
-        Rock rock2 = new Rock();
+        Cell cell2 = new Cell();
         Location loc2 = new Location(Y2, X2);
-        grid.put(loc2, rock2);
+        grid.put(loc2, cell2);
         
-        Rock rock3 = new Rock();
+        Cell cell3 = new Cell();
         Location loc3 = new Location(Y3, X3);
-        grid.put(loc3, rock3);
+        grid.put(loc3, cell3);
         
-        Rock rock4 = new Rock();
+        Cell cell4 = new Cell();
         Location loc4 = new Location(Y4, X4);
-        grid.put(loc4, rock4);
+        grid.put(loc4, cell4);
         
-        Rock rock5 = new Rock();
+        Cell cell5 = new Cell();
         Location loc5 = new Location(Y5, X5);
-        grid.put(loc5, rock5);
+        grid.put(loc5, cell5);
         
-        Rock rock6 = new Rock();
+        Cell cell6 = new Cell();
         Location loc6 = new Location(Y6, X6);
-        grid.put(loc6, rock6);
+        grid.put(loc6, cell6);
         
-        Rock rock7= new Rock();
+        Cell cell7= new Cell();
         Location loc7 = new Location(Y7, X7);
-        grid.put(loc7, rock7);
+        grid.put(loc7, cell7);
         
-        Rock rock8 = new Rock();
+        Cell cell8 = new Cell();
         Location loc8 = new Location(Y8, X8);
-        grid.put(loc8, rock8);
+        grid.put(loc8, cell8);
         
-        Rock rock9 = new Rock();
+        Cell cell9 = new Cell();
         Location loc9 = new Location(Y9, X9);
-        grid.put(loc9, rock9);
+        grid.put(loc9, cell9);
         
-        Rock rock10 = new Rock();
+        Cell cell10 = new Cell();
         Location loc10 = new Location(Y10, X10);
-        grid.put(loc10, rock10);
+        grid.put(loc10, cell10);
         
-        Rock rock11 = new Rock();
+        Cell cell11 = new Cell();
         Location loc11 = new Location(Y11, X11);
-        grid.put(loc11, rock11);
+        grid.put(loc11, cell11);
         
-        Rock rock12 = new Rock();
+        Cell cell12 = new Cell();
         Location loc12 = new Location(Y12, X12);
-        grid.put(loc12, rock12);
+        grid.put(loc12, cell12);
         
-        Rock rock13 = new Rock();
+        Cell cell13 = new Cell();
         Location loc13 = new Location(Y13, X13);
-        grid.put(loc13, rock13);
+        grid.put(loc13, cell13);
         
-        Rock rock14 = new Rock();
+        Cell cell14 = new Cell();
         Location loc14 = new Location(Y14, X14);
-        grid.put(loc14, rock14);
+        grid.put(loc14, cell14);
         
-        Rock rock15 = new Rock();
+        Cell cell15 = new Cell();
         Location loc15 = new Location(Y15, X15);
-        grid.put(loc15, rock15);
+        grid.put(loc15, cell15);
         
-        Rock rock16 = new Rock();
+        Cell cell16 = new Cell();
         Location loc16 = new Location(Y16, X16);
-        grid.put(loc16, rock16);
+        grid.put(loc16, cell16);
         
-        Rock rock17 = new Rock();
+        Cell cell17 = new Cell();
         Location loc17 = new Location(Y17, X17);
-        grid.put(loc17, rock17);
+        grid.put(loc17, cell17);
         
-        Rock rock18 = new Rock();
+        Cell cell18 = new Cell();
         Location loc18 = new Location(Y18, X18);
-        grid.put(loc18, rock18);
+        grid.put(loc18, cell18);
         
-        Rock rock19 = new Rock();
+        Cell cell19 = new Cell();
         Location loc19 = new Location(Y19, X19);
-        grid.put(loc19, rock19);
+        grid.put(loc19, cell19);
         
-        Rock rock20 = new Rock();
+        Cell cell20 = new Cell();
         Location loc20 = new Location(Y20, X20);
-        grid.put(loc20, rock20);
+        grid.put(loc20, cell20);
         
-        Rock rock21 = new Rock();
+        Cell cell21 = new Cell();
         Location loc21 = new Location(Y21, X21);
-        grid.put(loc21, rock21);
+        grid.put(loc21, cell21);
         
-        Rock rock22 = new Rock();
+        Cell cell22 = new Cell();
         Location loc22 = new Location(Y22, X22);
-        grid.put(loc22, rock22);
+        grid.put(loc22, cell22);
         
-        Rock rock23 = new Rock();
+        Cell cell23 = new Cell();
         Location loc23 = new Location(Y23, X23);
-        grid.put(loc23, rock23);
+        grid.put(loc23, cell23);
         
-        Rock rock24 = new Rock();
+        Cell cell24 = new Cell();
         Location loc24 = new Location(Y24, X24);
-        grid.put(loc24, rock24);
+        grid.put(loc24, cell24);
         
-        Rock rock25 = new Rock();
+        Cell cell25 = new Cell();
         Location loc25 = new Location(Y25, X25);
-        grid.put(loc25, rock25);
+        grid.put(loc25, cell25);
         
-        Rock rock26 = new Rock();
+        Cell cell26 = new Cell();
         Location loc26 = new Location(Y26, X26);
-        grid.put(loc26, rock26);
+        grid.put(loc26, cell26);
     }
 
     /**
@@ -214,13 +215,13 @@ public class GameOfLife
                 for (int column = 0; column <= 19; column++)
                 {
                     Location loc = new Location(row, column);
-                    Rock rockNew = new Rock();
+                    Cell cellNew = new Cell();
                     if (this.getActor(row, column) == null)
                     {
                       int neighbors = (grid.getOccupiedAdjacentLocations(loc)).size();
                       if (neighbors ==3)
                       {
-                          gridNew.put(loc, rockNew);
+                          gridNew.put(loc, cellNew);
                       }
                     }
                     else
@@ -228,7 +229,7 @@ public class GameOfLife
                       int neighbors = (grid.getOccupiedAdjacentLocations(loc)).size();
                       if (neighbors == 2 || neighbors == 3)
                       {
-                          gridNew.put(loc, rockNew);
+                          gridNew.put(loc, cellNew);
                       }
                     }
                 }
@@ -280,13 +281,13 @@ public class GameOfLife
         GameOfLife game = new GameOfLife();
         
         Scanner s = new Scanner(System.in);
-        System.out.print("How many generations would you like to see? ");
+        System.out.print("How many new generations would you like to see? ");
         int generations = s.nextInt();
         
         for (int i = 1; i <= generations; i++)
         {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(750);
             } catch (InterruptedException e) {}
             game.createNextGeneration();
         }
